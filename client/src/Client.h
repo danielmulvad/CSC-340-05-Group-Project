cf8b8fb57fdf264e5893c25dc93c8a63b90a60d9
Daniel Mulvad <daniel@dmulvad.com>
2023-11-15 20:00:51 -0800

    Refactor socket setup and handle client connections

--- client/src/Client.h
+++ client/src/Client.h
@@ -1,25 +1,52 @@
 #ifndef _CLIENT_H_
 #define _CLIENT_H_
 
+#include "../../common/Message.h"
 #include <arpa/inet.h>
 #include <stdexcept>
 #include <stdio.h>
 #include <string>
 #include <sys/socket.h>
 #include <unistd.h>
-#include "../../common/Message.h"
 
 class Client
 {
 private:
-    const unsigned int port = 8080;
+    const unsigned int port;
+    int socket_fd;
+
+    void setupConnection()
+    {
+        struct sockaddr_in serv_addr;
+        socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (socket_fd < 0)
+        {
+            perror("Socket creation error");
+            exit(EXIT_FAILURE);
+        }
+
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(port);
+
+        if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+        {
+            perror("Invalid address/ Address not supported");
+            exit(EXIT_FAILURE);
+        }
+
+        if (connect(socket_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        {
+            perror("Connection Failed");
+            exit(EXIT_FAILURE);
+        }
+    }
 
 public:
-    Client(const unsigned int &port) : port(port)
+    Client(unsigned int port) : port(port)
     {
-        if (port > 65565)
+        if (port > 65535)
         {
-            throw std::invalid_argument("Port must be less than 65565");
+            throw std::invalid_argument("Port must be less than 65535");
         }
         else if (port < 1024)
         {
@@ -29,52 +56,27 @@ public:
 
     int run()
     {
+        setupConnection();
 
-        int status, valread, client_fd;
-        struct sockaddr_in serv_addr;
-        std::string hello = "Hello from client";
-        if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-        {
-            printf("\n Socket creation error \n");
-            return -1;
-        }
-
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_port = htons(this->port);
-
-        // Convert IPv4 and IPv6 addresses from text to binary form
-        if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
-        {
-            printf(
-                "\nInvalid address/ Address not supported \n");
-            return -1;
-        }
-
-        if ((status = connect(client_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))) < 0)
-        {
-            printf("\nConnection Failed \n");
-            return -1;
-        }
-
-        std::string currentTimestamp = getCurrentTimestamp();
-        Message helloMsg(currentTimestamp, "Hello from client");
+        Message helloMsg(getCurrentTimestamp(), "Hello from client");
         std::string serializedMsg = helloMsg.serialize();
-        send(client_fd, serializedMsg.c_str(), serializedMsg.length(), 0);
+        send(socket_fd, serializedMsg.c_str(), serializedMsg.length(), 0);
         printf("Hello message sent\n");
 
         char buffer[1024] = {0};
-        valread = read(client_fd, buffer, 1024 - 1);
+        ssize_t valread = read(socket_fd, buffer, sizeof(buffer) - 1);
         if (valread < 0)
         {
             perror("read");
             exit(EXIT_FAILURE);
         }
-        Message receivedMsg = Message::deserialize(std::string(buffer));
-        printf("%s\n", receivedMsg.content.c_str());
 
-        close(client_fd);
+        Message receivedMsg = Message::deserialize(std::string(buffer));
+        printf("Received: %s\n", receivedMsg.content.c_str());
+
+        close(socket_fd);
         return 0;
-    };
+    }
 };
 
 #endif
--- server/src/Server.h
+++ server/src/Server.h
@@ -1,7 +1,9 @@
 #ifndef _SERVER_H_
 #define _SERVER_H_
 
+#include "../../common/Message.h"
 #include <netinet/in.h>
+#include <signal.h>
 #include <stdexcept>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,56 +11,38 @@
 #include <string>
 #include <sys/socket.h>
 #include <unistd.h>
-#include "../../common/Message.h"
+
+void signalHandler(int signum);
 
 class Server
 {
 private:
-    const unsigned int port = 8080;
+    const unsigned int port;
+    int server_fd;
 
-public:
-    Server(const unsigned int &port) : port(port)
+    void setupSocket()
     {
-        if (port > 65565)
-        {
-            throw std::invalid_argument("Port must be less than 65565");
-        }
-        else if (port < 1024)
-        {
-            throw std::invalid_argument("Port must be greater than 1024");
-        }
-    }
-
-    int run()
-    {
-        int server_fd, new_socket;
         struct sockaddr_in address;
         int opt = 1;
-        socklen_t addrlen = sizeof(address);
-        std::string hello = "Hello from server";
 
-        // Creating socket file descriptor
-        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        server_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (server_fd < 0)
         {
             perror("socket failed");
             exit(EXIT_FAILURE);
         }
 
-        // Attaching socket to the port 8080
-        if (setsockopt(server_fd, SOL_SOCKET,
-                       SO_REUSEADDR, &opt,
-                       sizeof(opt)))
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
         {
             perror("setsockopt");
             exit(EXIT_FAILURE);
         }
+
         address.sin_family = AF_INET;
         address.sin_addr.s_addr = INADDR_ANY;
-        address.sin_port = htons(this->port);
+        address.sin_port = htons(port);
 
-        // Forcefully attaching socket to the port 8080
-        if (bind(server_fd, (struct sockaddr *)&address,
-                 sizeof(address)) < 0)
+        if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
         {
             perror("bind failed");
             exit(EXIT_FAILURE);
@@ -69,32 +53,61 @@ public:
             perror("listen");
             exit(EXIT_FAILURE);
         }
+    }
 
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0)
-        {
-            perror("accept");
-            exit(EXIT_FAILURE);
-        }
-
+    void handleClient(int clientSocket)
+    {
         char buffer[1024] = {0};
-        ssize_t valread = read(new_socket, buffer, 1024 - 1);
+        ssize_t valread = read(clientSocket, buffer, sizeof(buffer) - 1);
         if (valread < 0)
         {
             perror("read");
             exit(EXIT_FAILURE);
         }
+
         Message receivedMsg = Message::deserialize(std::string(buffer));
-        printf("%s\n", receivedMsg.content.c_str());
+        printf("Received: %s\n", receivedMsg.content.c_str());
 
-        std::string currentTimestamp = getCurrentTimestamp();
-        Message helloMsg(currentTimestamp, "Hello from server");
-        std::string serializedMsg = helloMsg.serialize();
-        send(new_socket, serializedMsg.c_str(), serializedMsg.length(), 0);
-        printf("Hello message sent\n");
+        Message replyMsg(getCurrentTimestamp(), "Hello from server");
+        std::string serializedMsg = replyMsg.serialize();
+        send(clientSocket, serializedMsg.c_str(), serializedMsg.length(), 0);
+        printf("Reply sent\n");
 
-        // closing the connected socket
-        close(new_socket);
-        // closing the listening socket
+        close(clientSocket);
+    }
+
+public:
+    Server(unsigned int port) : port(port)
+    {
+        if (port > 65535)
+        {
+            throw std::invalid_argument("Port must be less than 65535");
+        }
+        else if (port < 1024)
+        {
+            throw std::invalid_argument("Port must be greater than 1024");
+        }
+    }
+
+    int run()
+    {
+        setupSocket();
+
+        while (true)
+        {
+            struct sockaddr_in address;
+            socklen_t addrlen = sizeof(address);
+
+            int new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
+            if (new_socket < 0)
+            {
+                perror("accept");
+                continue; // Continue accepting other connections even if one fails
+            }
+
+            handleClient(new_socket);
+        }
+
         close(server_fd);
         return 0;
     }
