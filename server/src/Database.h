5d47c3359809bb3277eaf600cdc7c1e959c5748f
Daniel Mulvad <daniel@dmulvad.com>
2023-12-04 10:19:11 -0800

    Server-side database authentication

---
+++ server/src/Database.h
@@ -0,0 +1,134 @@
+#ifndef _DATABASE_H_
+#define _DATABASE_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
+
+class DatabaseItem
+{
+public:
+    std::string serialize();
+    static DatabaseItem deserialize(const std::string &);
+};
+
+class User : DatabaseItem
+{
+private:
+    std::string username;
+    std::string password;
+
+public:
+    User(std::string username, std::string password) : username(username), password(password) {}
+
+    std::string getUsername() const { return username; }
+    void setUsername(std::string username) { this->username = username; }
+    std::string getPassword() const { return password; }
+    void setPassword(std::string password) { this->password = password; }
+
+    std::string serialize() const
+    {
+        return username + ";" + password;
+    }
+
+    static User deserialize(const std::string &str)
+    {
+        std::string username, password;
+
+        std::istringstream iss(str);
+        std::getline(iss, username, ';');
+        std::getline(iss, password, ';');
+
+        std::cout << username << std::endl;
+
+        return User(username, password);
+    }
+};
+
+class Database
+{
+private:
+    std::string path;
+    std::vector<User> data;
+
+public:
+    Database() : Database("./database.txt") {}
+    Database(std::string path) : path(path)
+    {
+        load();
+    }
+
+    bool add(std::string username, std::string password)
+    {
+        if (!contains(username))
+        {
+            data.push_back(User(username, password));
+            save();
+            return true;
+        }
+        return false;
+    }
+
+    bool contains(std::string username)
+    {
+        return std::any_of(data.begin(), data.end(), [&username](const User &user)
+                           { return user.getUsername() == username; });
+    }
+
+    bool contains(std::string username, std::string password)
+    {
+        return std::any_of(data.begin(), data.end(), [&username, &password](const User &user)
+                           { return user.getUsername() == username && user.getPassword() == password; });
+    }
+
+    void remove(std::string username)
+    {
+        data.erase(std::remove_if(data.begin(), data.end(), [&username](const User &user)
+                                  { return user.getUsername() == username; }),
+                   data.end());
+        save();
+    }
+
+    void update(std::string username, std::string password)
+    {
+        auto it = std::find_if(data.begin(), data.end(), [&username](const User &user)
+                               { return user.getUsername() == username; });
+        if (it != data.end())
+        {
+            it->setPassword(password);
+        }
+        save();
+    }
+
+    void save()
+    {
+        std::ofstream file(path);
+        if (file.is_open())
+        {
+            for (const auto &user : data)
+            {
+                file << user.serialize() << std::endl;
+            }
+            file.close();
+        }
+    }
+
+    void load()
+    {
+        std::ifstream file(path);
+        if (file.is_open())
+        {
+            std::string line;
+            while (std::getline(file, line))
+            {
+                data.push_back(User::deserialize(line));
+            }
+            file.close();
+        }
+    }
+};
+
+#endif // _DATABASE_H_
--- server/src/Server.h
+++ server/src/Server.h
@@ -2,6 +2,7 @@
 #define _SERVER_H_
 
 #include "../../common/Message.h"
+#include "./Database.h"
 #include "./ServerMessenger.h"
 
 void signalHandler(int signum);
@@ -9,19 +10,23 @@ void signalHandler(int signum);
 class Server
 {
 private:
-    ServerMessenger messenger;
+    Database *database;
+    ServerMessenger *messenger;
 
 public:
     Server() : Server(8080) {}
-    Server(unsigned int port) : messenger(port) {}
+    Server(unsigned int port) : database(new Database()), messenger(new ServerMessenger(port)) {}
+    ~Server()
+    {
+        delete database;
+        delete messenger;
+    }
 
     void handleEstablishConnectionRequest(const int &connectionId, const Message &msg)
     {
         printf("Received message: %s\n", msg.serialize().c_str());
         EstablishConnectionResponseMessage response(connectionId);
-        messenger.sendMessageToClient(connectionId, response);
-
-        // TODO: Add connection to Messenger LinkedList<int> *connectedClients;
+        messenger->sendMessageToClient(connectionId, response);
 
         return;
     }
@@ -30,77 +35,120 @@ public:
     {
         printf("Received message: %s\n", msg.serialize().c_str());
         DropConnectionResponseMessage response(connectionId);
-        messenger.sendMessageToClient(connectionId, response);
-
-        // TODO: Remove connection from Messenger LinkedList<int> *connectedClients;
-
+        messenger->sendMessageToClient(connectionId, response);
         return;
     }
 
     void handleRegisterRequest(const int &connectionId, const Message &msg)
     {
         printf("Received message: %s\n", msg.serialize().c_str());
-        std::string username, password;
-        std::istringstream iss(msg.content);
-        iss >> username >> password;
-        if (username.empty() || password.empty())
+        std::vector<std::string> parts;
+        std::stringstream ss(msg.content);
+        std::string part;
+
+        while (std::getline(ss, part, ' '))
         {
-            printf("Invalid username or password\n");
+            parts.push_back(part);
+        }
+
+        const size_t expectedParts = 3;
+        if (parts.size() != expectedParts)
+        {
+            printf("Invalid serialized message format for %s\n", msg.serialize().c_str());
+            RegisterResponseMessage response(connectionId, RegisterResponseCode::REGISTER_FAILED);
+            messenger->sendMessageToClient(connectionId, response);
             return;
         }
 
-        // TODO: Save username/password to file
+        std::string username = parts[1];
+        std::string password = parts[2];
+        if (username.empty() || password.empty())
+        {
+            printf("Invalid username or password\n");
+            RegisterResponseMessage response(connectionId, RegisterResponseCode::REGISTER_FAILED);
+            messenger->sendMessageToClient(connectionId, response);
+            return;
+        }
+
+        bool ok = this->database->add(username, password);
+        if (!ok)
+        {
+            RegisterResponseMessage response(connectionId, RegisterResponseCode::REGISTER_FAILED);
+            messenger->sendMessageToClient(connectionId, response);
+            return;
+        }
 
         RegisterResponseMessage response(connectionId, RegisterResponseCode::REGISTER_SUCCESS);
-        messenger.sendMessageToClient(connectionId, response);
-        return;
+        messenger->sendMessageToClient(connectionId, response);
     }
 
     void handleLoginRequest(const int &connectionId, const Message &msg)
     {
         printf("Received message: %s\n", msg.serialize().c_str());
-        std::string username, password;
-        std::istringstream iss(msg.content);
-        iss >> username >> password;
-        if (username.empty() || password.empty())
+
+        std::vector<std::string> parts;
+        std::stringstream ss(msg.content);
+        std::string part;
+
+        while (std::getline(ss, part, ' '))
         {
-            printf("Invalid username or password\n");
+            parts.push_back(part);
+        }
+
+        const size_t expectedParts = 3;
+        if (parts.size() != expectedParts)
+        {
+            printf("Invalid serialized message format for %s\n", msg.serialize().c_str());
+            LoginResponseMessage response(connectionId, LoginResponseCode::LOGIN_FAILED);
+            messenger->sendMessageToClient(connectionId, response);
             return;
         }
 
-        // TODO: Check username/password from file
+        std::string username = parts[1];
+        std::string password = parts[2];
+        if (username.empty() || password.empty())
+        {
+            printf("Invalid username or password\n");
+            LoginResponseMessage response(connectionId, LoginResponseCode::LOGIN_FAILED);
+            messenger->sendMessageToClient(connectionId, response);
+            return;
+        }
+        bool ok = this->database->contains(username, password);
+        if (!ok)
+        {
+            LoginResponseMessage response(connectionId, LoginResponseCode::LOGIN_FAILED);
+            messenger->sendMessageToClient(connectionId, response);
+            return;
+        }
 
         LoginResponseMessage response(connectionId, LoginResponseCode::LOGIN_SUCCESS);
-        messenger.sendMessageToClient(connectionId, response);
-        return;
+        messenger->sendMessageToClient(connectionId, response);
     }
-
-    ~Server() {}
 
     int start()
     {
-        std::string establish_connection_response_handler_path = CLIENT_TO_SERVER_PREFIX + " " + CLIENT_ESTABLISH_CONNECTION_REQUEST_PREFIX;
+        std::string establish_connection_response_handler_path = CLIENT_ESTABLISH_CONNECTION_REQUEST_PREFIX;
         auto establish_connection_response_handler = std::bind(&Server::handleEstablishConnectionRequest, this, std::placeholders::_1, std::placeholders::_2);
-        messenger.registerHandler(establish_connection_response_handler_path, establish_connection_response_handler);
+        messenger->registerHandler(establish_connection_response_handler_path, establish_connection_response_handler);
 
-        std::string drop_connection_response_handler_path = CLIENT_TO_SERVER_PREFIX + " " + CLIENT_DROP_CONNECTION_REQUEST_PREFIX;
+        std::string drop_connection_response_handler_path = CLIENT_DROP_CONNECTION_REQUEST_PREFIX;
         auto drop_connection_response_handler = std::bind(&Server::handleDropConnectionRequest, this, std::placeholders::_1, std::placeholders::_2);
-        messenger.registerHandler(drop_connection_response_handler_path, drop_connection_response_handler);
+        messenger->registerHandler(drop_connection_response_handler_path, drop_connection_response_handler);
 
-        std::string register_response_handler_path = CLIENT_TO_SERVER_PREFIX + " " + CLIENT_REGISTER_REQUEST_PREFIX;
+        std::string register_response_handler_path = CLIENT_REGISTER_REQUEST_PREFIX;
         auto register_response_handler = std::bind(&Server::handleRegisterRequest, this, std::placeholders::_1, std::placeholders::_2);
-        messenger.registerHandler(register_response_handler_path, register_response_handler);
+        messenger->registerHandler(register_response_handler_path, register_response_handler);
 
-        std::string login_response_handler_path = CLIENT_TO_SERVER_PREFIX + " " + CLIENT_LOGIN_REQUEST_PREFIX;
+        std::string login_response_handler_path = CLIENT_LOGIN_REQUEST_PREFIX;
         auto login_response_handler = std::bind(&Server::handleLoginRequest, this, std::placeholders::_1, std::placeholders::_2);
-        messenger.registerHandler(login_response_handler_path, login_response_handler);
+        messenger->registerHandler(login_response_handler_path, login_response_handler);
 
-        return messenger.start();
+        return messenger->start();
     }
 
     int stop()
     {
-        return messenger.stop();
+        return messenger->stop();
     }
 };
 
